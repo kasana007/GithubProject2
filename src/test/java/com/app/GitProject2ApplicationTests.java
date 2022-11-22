98a4ef1e77a8d61b96d19c8bba7dd026e27b2fed
TL <kasanaamit29@gmail.com>
2022-11-22 11:50:11 +0530

    Commited by Amit

---
+++ src/test/java/com/app/GitProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
